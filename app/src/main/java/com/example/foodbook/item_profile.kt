dad54bd1fcb9b999c85804d2106856104a2c258a
aader320 <han.domain1@gmail.com>
2024-02-21 04:37:14 +0800

    (map button + fragment)

--- app/src/main/java/com/example/foodbook/item_profile.kt
+++ app/src/main/java/com/example/foodbook/item_profile.kt
@@ -2,10 +2,27 @@ package com.example.foodbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
 
 class item_profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_profile)
+
+        val buttonOpenFragment = findViewById<Button>(R.id.loc)
+        buttonOpenFragment.setOnClickListener {
+            // Replace 'YourFragment' with the fragment you want to open
+            replaceFragment(MapsFragment())
+
+        }
+
     }
+
+        private fun replaceFragment(fragment: Fragment) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_map, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.13" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
